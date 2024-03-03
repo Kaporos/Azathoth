76d70edab52a9b28f8c1bd86c39c11f29eb29327
Kaporos <theo@daron.be>
2024-03-03 12:58:35 +0100

    refactor: ♻️ making view clearer

--- app/app.go
+++ app/app.go
@@ -20,14 +20,20 @@ type Model struct {
 	input    string
 }
 
+// This Init will update m , the bubbletea Init below cannot change m because of its signature
 func (m *Model) InitMutable() {
+	//Giving the player basics items at log-in
 	m.player.GiveItem(stores.CreateItemSure("apple"))
 	m.player.GiveItem(stores.CreateItemSure("skin"))
 }
+
+// bubbletea init method. required to have but it does not do anything.
 func (m Model) Init() tea.Cmd {
 	return nil
 }
 
+// bubbletea update method. called at each event (inside of msg var) and returns updated model, as well as tea commands
+// tea commands are related to IO stuff, we don't use them ATM (except for disconnecting player on quit)
 func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 	case tea.WindowSizeMsg:
@@ -82,19 +88,30 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	return m, nil
 }
 
+// bubbletea view method. It generates what the player sees.
 func (m Model) View() string {
 	map_view := components.RenderMap(&m.player)
 	player_stats := components.RenderPlayerStats(&m.player)
-	history_style := m.TxtStyle.MarginLeft(5).MarginTop(1)
+	left_style := m.TxtStyle.MarginLeft(5).MarginTop(1)
 
-	//Computing size of visible history (to avoid overflow)
+	// Computing size of visible history (to avoid overflow)
 	size := len(strings.Split(m.History, "\n")) - m.Height*2/3
 	if size < 0 {
 		size = 0
 	}
 
+	right_side := map_view + "\n\n\n" + player_stats
+
+	//stripping history to only keep max 2/3 of terminal's height.
+	left_side := strings.Join(strings.Split(m.History, "\n")[size:], "\n")
+
+	//adding user prompt
+	left_side += components.RenderPrompt(m.input, true)
+
 	return lipgloss.JoinHorizontal(
+		//We want to align views at top
 		lipgloss.Top,
-		map_view+"\n\n\n"+player_stats,
-		history_style.Render(strings.Join(strings.Split(m.History, "\n")[size:], "\n")+components.RenderPrompt(m.input, true)))
+		right_side,
+		left_style.Render(left_side),
+	)
 }
